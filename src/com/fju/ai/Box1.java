d907a2734ad96c4fb5a244be9c1a78af660d1ddd
amandakuan <amandakuan6@gmail.com>
2022-04-29 10:55:31 +0800

    4/29 Box problem solved

---
+++ src/com/fju/ai/Box1.java
@@ -0,0 +1,12 @@
+package com.fju.ai;
+
+public class Box1 extends Box{
+    public Box1() {
+        super(10, 10, 10);
+    }
+
+    @Override
+    public String toString() {
+        return "no1";
+    }
+}
---
+++ src/com/fju/ai/Box2.java
@@ -0,0 +1,12 @@
+package com.fju.ai;
+
+public class Box2 extends Box{
+    public Box2() {
+        super(20, 20, 20);
+    }
+
+    @Override
+    public String toString() {
+        return "no2";
+    }
+}
